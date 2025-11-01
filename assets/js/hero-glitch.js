d5557e5b79e4f31def3d261628984adc0a68a082
AOM-laboratory <aorcajadameseguer@gmail.com>
2025-11-01 13:32:03 +0100

    Update hero-glitch.js

--- assets/js/hero-glitch.js
+++ assets/js/hero-glitch.js
@@ -15,7 +15,7 @@
 
   let i=0, j=0;
 
-  const delayBeforeName = 1600; // deja parpadear 2 veces antes de escribir
+  const delayBeforeName = 700; // deja parpadear 2 veces antes de escribir
   setTimeout(typeName, delayBeforeName);
 
   function typeName(){
